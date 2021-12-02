6a281993ef44e995bf6caa4734148c9e0fb402c3
JustJindo <elee.jindo@gmail.com>
2021-12-02 11:40:49 -0700

    Added 'inventory' function to Cookie.

--- Cookie - Recipe Retrieval.py
+++ Cookie - Recipe Retrieval.py
@@ -1,9 +1,19 @@
+def inventory(ingredient):
+	if ingredient == []:
+		print('Starve.')
+	else:
+		for i in range(len(ingredient)):
+			print(f"{i + 1}. {ingredient[i]}")
+			i += 1
+
 open_recipes = ['aglio.txt', 'carbonara.txt', 'chicknoodle.txt']
 
 number = 1
 for recipe in open_recipes:
 	print(str(number) + '.', recipe[:-4])
 	number += 1
+
+inventory(open_recipes)
 
 choice = input()
 with open(open_recipes[choice - 1], 'r') as f:
--- Cookie.py
+++ Cookie.py
@@ -1,12 +1,28 @@
-ingredient_selection = ['1. onions', '2. garlic', '3. olive oil', '4. eggs', '5. chicken stock']
+# Takes in a list and displays a numbered inventory of its elements.
+def inventory(ingredient):
+	if ingredient == []:
+		print('Starve.')
+	else:
+		for i in range(len(ingredient)):
+			print(f"{i + 1}. {ingredient[i]}")
+			i += 1
+
+# A master list of recipes in .txt files.
+recipes = ['aglio.txt', 'stir_fry.txt', 'carbonara.txt', 'chicken_noodle.txt']
+# A master list of ingredients.
+ingredient_selection = ['onions', 'garlic', 'olive oil', 'eggs', 'chicken stock', 'chicken (breast/thighs)', 'pasta', 'vegetables', \
+'bacon', 'parmesan', 'honey', 'soy sauce']
+# A list to store ingredients on hand.
 ingredients = []
 
+# Display instructions for how to use Cookie.
 print("""Of these ingredients, which ones do you currently have?
 ________________________________________________
 
-If you have an ingredient, type the number associated with it ('1' for onions).
+If you have an ingredient, type the number associated with it ('1' for '1. [ingredient]').
 If you're done listing ingredients, type 'Done'.
 ________________________________________________
 """)
-for ingredient in ingredient_selection:
-	print(ingredient)
+
+# Call on 'inventory' to list ingredients.
+inventory(ingredient_selection)
