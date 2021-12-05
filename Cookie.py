0d9a7de817f09ac5f34dd30684266b1901162e2a
JustJindo <elee.jindo@gmail.com>
2021-12-04 20:01:42 -0700

    Cookie can determine what recipes the user can cook.

--- Cookie.py
+++ Cookie.py
@@ -9,9 +9,10 @@ def inventory(ingredient):
 
 # A master list of recipes in .txt files.
 recipes = ['aglio.txt', 'stir_fry.txt', 'carbonara.txt', 'chicken_noodle.txt']
+names = ['Pasta Aglio e Olio', 'Stir Fry', 'Pasta Carbonara', 'Chicken Noodle Soup']
 # A master list of ingredients.
-ingredient_selection = ['onions', 'garlic', 'olive oil', 'eggs', 'chicken stock', 'chicken (breast/thighs)', 'pasta', 'vegetables', \
-'bacon', 'parmesan', 'honey', 'soy sauce']
+ingredient_selection = ['onions', 'garlic', 'olive oil', 'eggs', 'stock', 'chicken', 'pasta', 'vegetables', \
+'bacon', 'parmesan', 'soy sauce']
 # A list to store ingredients on hand.
 ingredients = []
 
@@ -22,7 +23,6 @@ ________________________________________________
 If you have an ingredient, type the number associated with it ('1' for '1. [ingredient]').
 If you're done listing ingredients, type 'Done'.
 ________________________________________________
-
 """)
 
 # Call on 'inventory' to list ingredients.
@@ -34,8 +34,10 @@ known = []
 
 # Take in and store ingredients on hand.
 check = ''
+# Parameter only serves to enable while loop.
 while check != 'Done':
 	check = input("... ")
+	# This check breaks the loop.
 	if check == 'Done':
 		break
 	else:
@@ -44,4 +46,46 @@ while check != 'Done':
 			known.append(check)
 			item = ingredient_selection[check - 1]
 			ingredients.append(item)
-print('')
+
+# Store cookable recipes in here.
+cookable = []
+# Use this list to record shared ingredients.
+shared = []
+
+# Takes in two lists and determines if they're the same.
+# Our lists are recipe ingredients and shared ingredients.
+def compare(first, second):
+	first.sort()
+	second.sort()
+	if first == second:
+		return 'Equal'
+	if first != second:
+		return 'Unequal'
+
+# Giant lists of recipe ingredients (same order as recipes list).
+cookbook = [['garlic', 'olive oil', 'pasta'], ['olive oil', 'stock', 'chicken', 'vegetables', 'soy sauce'], \
+['eggs', 'pasta', 'bacon', 'parmesan']]
+
+# Go through each recipe in cookbook.
+for entry in cookbook:
+	reference = cookbook.index(entry)
+	recipe = names[reference]
+	# Go through each ingredient in recipe.
+	for requirement in entry:
+		# If user has the ingredient, add it to shared list.
+		if requirement in ingredients:
+			shared.append(requirement)
+	# Compare recipe with shared list.
+	cookability = compare(entry, shared)
+	if cookability == 'Equal':
+		cookable.append(recipe)
+
+print("""________________________________________________
+
+These are the recipes you can make with what you have.
+To choose a recipe, type the number associated with it ('1' for '1. [recipe]').
+________________________________________________
+""")
+
+# Call on 'inventory' to list cookable recipes.
+inventory(cookable)
